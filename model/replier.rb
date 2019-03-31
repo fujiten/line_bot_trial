58934f1b18232146ce41fcf11ebd9dfd04dd3222
hikaru <hikaru.fujita.0903@gmail>
2019-03-31 17:27:30 +0900

    Add_a_User_Status_called_Fighting(Fightingというユーザーのステータスを追加しました)

--- model/replier.rb
+++ model/replier.rb
@@ -62,64 +62,62 @@ class Replier
         client.reply_message(events[0]['replyToken'], message)
       end
 
+      if status == "fighting"
+        message = {
+          type: 'text',
+          text: "[状態：戦闘中]\n#{text_params}…？私には効かんなあ！",
+          quickReply: BattleChoice.create_quick_reply_object
+        }
+        client.reply_message(events[0]['replyToken'], message)
+      end
 
 
       if status == "0"
-        events.each { |event|
-          case event
-          when Line::Bot::Event::Message
-            case event.type
-            when Line::Bot::Event::MessageType::Text
-              if text_params =~ /天気/
-                status = "天気"
-                user.status = status
-                user.save
-                message = {
-                  type: 'text',
-                  text: "[状態：天気検索] \n今日の天気を調べるよ。知りたい場所を入力してみて。"
-                }
-              elsif text_params =~ /スライム/
-                template = {
-                  type: 'buttons',
-                  text: 'スライムがあらわれた！',
-                  thumbnailImageUrl: 'https://item-shopping.c.yimg.jp/i/j/fishingcat_5958',
-                  actions: [
-                             { type: "postback", label: 'たたかう', data: 'たたかう' },
-                             { type: 'postback', label: 'にげる',  data: 'にげる' }
-                             ]
-                  }
-                message = {
-                  type: 'template',
-                  altText: '代替テキスト',
-                  template: template
-                }
-              elsif text_params =~ /ニュース/
-                news = News.new
-                news_info = news.fetch_top_access_of_news
-                message = {
-                  type: 'text',
-                  text: "今日のトップニュースは「#{news_info[:title]}」です。
-                  詳細は#{news_info[:link]}へどうぞ。（情報元：#{news_info[:source]}）"
-                }
-              else
-                message = {
-                  type: 'text',
-                  text: "[状態：待機中]\n#{event.message['text']}…って、どういう意味？",
-                  quickReply: BattleChoice.create_quick_reply_object
-                }
-              end
-              client.reply_message(event['replyToken'], message)
-            when Line::Bot::Event::MessageType::Image, Line::Bot::Event::MessageType::Video
-              response = client.get_message_content(event.message['id'])
-              tf = Tempfile.open("content")
-              tf.write(response.body)
-            end
-          end
-        }
+        if text_params =~ /天気/
+          status = "天気"
+          user.status = status
+          user.save
+          message = {
+            type: 'text',
+            text: "[状態：天気検索] \n今日の天気を調べるよ。知りたい場所を入力してみて。"
+          }
+        elsif text_params =~ /スライム/
+          template = {
+            type: 'buttons',
+            text: 'スライムがあらわれた！',
+            thumbnailImageUrl: 'https://item-shopping.c.yimg.jp/i/j/fishingcat_5958',
+            actions: [
+                       { type: "postback", label: 'たたかう', data: 'たたかう' },
+                       { type: 'postback', label: 'にげる',  data: 'にげる' }
+                       ]
+            }
+          message = {
+            type: 'template',
+            altText: '代替テキスト',
+            template: template
+          }
+        elsif text_params =~ /ニュース/
+          news = News.new
+          news_info = news.fetch_top_access_of_news
+          message = {
+            type: 'text',
+            text: "今日のトップニュースは「#{news_info[:title]}」です。
+            詳細は#{news_info[:link]}へどうぞ。（情報元：#{news_info[:source]}）"
+          }
+        else
+          message = {
+            type: 'text',
+            text: "[状態：待機中]\n#{text_params}…って、どういう意味？",
+            quickReply: BattleChoice.create_quick_reply_object
+          }
+        end
+        client.reply_message(events[0]['replyToken'], message)
       end
     else
       data_params = postback_params["data"]
       if data_params == 'たたかう'
+        user.status = "fighting"
+        user.save
         message = {
           type: 'text',
           text: "[状態：戦闘開始！]",
